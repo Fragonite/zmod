3c4f5f7c084c9e9f8879fa9adb46a1271d3a590f
Fragonite <48802408+Fragonite@users.noreply.github.com>
2023-05-10 21:26:57 +0800

    Add files via upload

--- zmod_dw8xlce_ultrawide.cpp
+++ zmod_dw8xlce_ultrawide.cpp
@@ -1,21 +1,43 @@
 #define WIN32_LEAN_AND_MEAN
-#include <Windows.h>
+#include <windows.h>
 #include <string>
 #include <filesystem>
 #include <map>
+#include <vector>
+#include <sstream>
 #include "d3d9.h"
-#include "zmod_dw8xlce_ultrawide.hpp"
 #include "detours.h"
-#include "io_helper.cpp"
+#include "zmod_common.cpp"
 
 struct
 {
     uint32_t width;
     uint32_t height;
-    double aspectRatio;
+    double aspect_ratio;
     bool windowed;
-    ini_map iniMap;
-} static globals;
+    zmod::ini_map ini;
+} globals;
+
+typedef IDirect3D9 *(WINAPI Direct3DCreate9_t)(UINT SDKVersion);
+Direct3DCreate9_t HookedDirect3DCreate9;
+
+typedef HRESULT(APIENTRY CreateDevice_t)(IDirect3D9 *pD3D9, UINT Adapter, D3DDEVTYPE DeviceType, HWND hFocusWindow, DWORD BehaviorFlags, D3DPRESENT_PARAMETERS *pPresentationParameters, IDirect3DDevice9 **ppReturnedDeviceInterface);
+CreateDevice_t HookedCreateDevice;
+
+typedef HRESULT(APIENTRY Reset_t)(IDirect3DDevice9 *pDevice, D3DPRESENT_PARAMETERS *pPresentationParameters);
+Reset_t HookedReset;
+
+typedef LSTATUS(WINAPI RegQueryValueExA_t)(HKEY hKey, LPCSTR lpValueName, LPDWORD lpReserved, LPDWORD lpType, LPBYTE lpData, LPDWORD lpcbData);
+RegQueryValueExA_t HookedRegQueryValueExA;
+
+typedef LSTATUS(WINAPI RegSetValueExA_t)(HKEY hKey, LPCSTR lpValueName, DWORD Reserved, DWORD dwType, const BYTE *lpData, DWORD cbData);
+RegSetValueExA_t HookedRegSetValueExA;
+
+typedef int(WINAPI GetSystemMetrics_t)(int nIndex);
+GetSystemMetrics_t HookedGetSystemMetrics;
+
+typedef void(__stdcall function_22DC70_t)(uint32_t a0, float *a1);
+function_22DC70_t HookedAspectRatio;
 
 Direct3DCreate9_t *_Direct3DCreate9 = (Direct3DCreate9_t *)GetProcAddress(GetModuleHandleA("d3d9.dll"), "Direct3DCreate9");
 CreateDevice_t *_CreateDevice = nullptr;
@@ -24,7 +46,6 @@ RegQueryValueExA_t *_RegQueryValueExA = RegQueryValueExA;
 RegSetValueExA_t *_RegSetValueExA = RegSetValueExA;
 GetSystemMetrics_t *_GetSystemMetrics = GetSystemMetrics;
 function_22DC70_t *_AspectRatio = nullptr;
-// function_1158A0_t *_AspectRatio = nullptr;
 
 IDirect3D9 *WINAPI HookedDirect3DCreate9(UINT SDKVersion)
 {
@@ -98,14 +119,6 @@ HRESULT APIENTRY HookedReset(IDirect3DDevice9 *pDevice, D3DPRESENT_PARAMETERS *p
     return _Reset(pDevice, pPresentationParameters);
 }
 
-void WriteMemory(void *address, void *data, size_t size)
-{
-    DWORD oldProtect;
-    VirtualProtect(address, size, PAGE_EXECUTE_READWRITE, &oldProtect);
-    memcpy(address, data, size);
-    VirtualProtect(address, size, oldProtect, &oldProtect);
-}
-
 int WINAPI HookedGetSystemMetrics(int nIndex)
 {
     if (nIndex == 0)
@@ -125,14 +138,90 @@ void __stdcall HookedAspectRatio(uint32_t a0, float *a1)
     {
         if (a1[6] == 360.0)
         {
-            globals.aspectRatio = (double)(globals.width * 2) / (double)globals.height;
+            globals.aspect_ratio = (double)(globals.width * 2) / (double)globals.height;
         }
         else
         {
-            globals.aspectRatio = (double)globals.width / (double)globals.height;
+            globals.aspect_ratio = (double)globals.width / (double)globals.height;
         }
     }
     _AspectRatio(a0, a1);
+}
+
+void module_main(HINSTANCE instance)
+{
+    auto module_path = zmod::get_module_path(instance);
+    auto ini_path = module_path.replace_extension(L".ini");
+    auto module_key = module_path.stem().wstring();
+
+    globals.ini = {
+        {{module_key, L"width"}, L"1280"},
+        {{module_key, L"height"}, L"720"},
+        {{module_key, L"fullscreen"}, L"0"},
+    };
+
+    if (!(zmod::file_exists(ini_path)))
+    {
+        zmod::write_ini_file(ini_path, globals.ini);
+    }
+    zmod::read_ini_file(ini_path, globals.ini);
+
+    globals.width = std::wcstoul(globals.ini[{module_key, L"width"}].c_str(), nullptr, 10);
+    globals.height = std::wcstoul(globals.ini[{module_key, L"height"}].c_str(), nullptr, 10);
+    globals.aspect_ratio = (double)globals.width / (double)globals.height;
+    globals.windowed = globals.ini[{module_key, L"fullscreen"}] != L"1";
+
+    struct patch
+    {
+        void *address;
+        void *data;
+        size_t size;
+    };
+
+    auto ar = zmod::parse_hex("DD 05 00 00 00 00");
+    {
+        auto p = &globals.aspect_ratio;
+        std::memcpy(ar.data() + 2, &p, 4);
+    }
+    float ar_sp_cutscene = globals.aspect_ratio;
+    float ar_mp_cutscene = globals.aspect_ratio * 2;
+
+    std::vector<patch> patches = {
+        {(void *)(zmod::find_pattern("C3 B8 00 05") + 2), &globals.width, 4},           // 1280 (Executable) Width
+        {(void *)(zmod::find_pattern("B8 00 04 00 00 6A 40") - 20), &globals.width, 4}, // 1280 (Executable) Window Width
+        {(void *)(zmod::find_pattern("B8 D0 02 00 00 5B") + 1), &globals.height, 4},    // 720 (Executable) Window Height
+        {(void *)(zmod::find_pattern("C7 44 24 0C 00 05") + 4), &globals.width, 4},     // 1280 (Executable) HUD Fix
+        {(void *)(zmod::find_pattern("10 00 05 00 00 EB 1C") + 1), &globals.width, 4},  // 1280 (Executable) Rage HUD Fix
+        {(void *)(zmod::find_pattern("80 07 00 00 E0 01 58 02")), &globals.width, 2},   // 1920 (Memory) Necessary
+        {(void *)(zmod::find_pattern("D9 43 14 D8 73 18")), ar.data(), ar.size()},      // Aspect Ratio
+        {(void *)(zmod::find_pattern("40 39 8E E3") + 1), &ar_sp_cutscene, 4},          // Single Player Cutscene Aspect Ratio
+        {(void *)(zmod::find_pattern("40 39 8E E3") + 9), &ar_mp_cutscene, 4},          // Multiplayer Cutscene Aspect Ratio
+    };
+
+    for (auto &p : patches)
+    {
+        zmod::write_memory(p.address, p.data, p.size);
+    }
+
+    _AspectRatio = (function_22DC70_t *)(zmod::find_pattern("33 C4 89 44 24 44 D9 05") - 8);
+
+    if (!(globals.windowed))
+    {
+        uint8_t hide_cursor_patch = 0x01;
+        zmod::write_memory((void *)(zmod::find_pattern("10 00 E8 ?? ?? ?? ?? 80 B8") + 13), &hide_cursor_patch, 1);
+    }
+
+    DetourTransactionBegin();
+    DetourUpdateThread(GetCurrentThread());
+    DetourAttach(&(PVOID &)_Direct3DCreate9, HookedDirect3DCreate9);
+    DetourAttach(&(PVOID &)_RegQueryValueExA, HookedRegQueryValueExA);
+    DetourAttach(&(PVOID &)_RegSetValueExA, HookedRegSetValueExA);
+    if (!(globals.windowed))
+    {
+        DetourAttach(&(PVOID &)_GetSystemMetrics, HookedGetSystemMetrics);
+    }
+    DetourAttach(&(PVOID &)_AspectRatio, HookedAspectRatio);
+    DetourTransactionCommit();
 }
 
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
@@ -142,70 +231,22 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
     case DLL_PROCESS_ATTACH:
         if (!(DisableThreadLibraryCalls(hinstDLL)))
             ;
+        module_main(hinstDLL);
+        // Initialize once for each new process.
+        // Return FALSE to fail DLL load.
+        break;
 
-        std::wstring moduleName;
-        moduleName.reserve(MAX_PATH);
-        if (!(GetModuleFileNameW(hinstDLL, moduleName.data(), moduleName.capacity())))
-            ;
-        auto modulePath = std::filesystem::path(moduleName.c_str());
-        auto iniPath = modulePath.replace_extension(L".ini");
-        std::wstring moduleKey = modulePath.stem().c_str();
+    case DLL_THREAD_ATTACH:
+        // Do thread-specific initialization.
+        break;
 
-        globals.iniMap = {
-            {{moduleKey, L"width"}, L"1280"},
-            {{moduleKey, L"height"}, L"720"},
-            {{moduleKey, L"fullscreen"}, L"0"},
-        };
+    case DLL_THREAD_DETACH:
+        // Do thread-specific cleanup.
+        break;
 
-        if (!(file_exists(iniPath)))
-        {
-            create_ini_file(iniPath, globals.iniMap);
-        }
-        read_ini_file(iniPath, globals.iniMap);
-
-        globals.width = std::wcstoul(globals.iniMap[{moduleKey, L"width"}].c_str(), nullptr, 10);
-        globals.height = std::wcstoul(globals.iniMap[{moduleKey, L"height"}].c_str(), nullptr, 10);
-        globals.aspectRatio = (double)globals.width / (double)globals.height;
-        globals.windowed = globals.iniMap[{moduleKey, L"fullscreen"}] != L"1";
-
-        auto base = (uint32_t)GetModuleHandleW(nullptr);
-        WriteMemory((void *)(base + 0x3753), &globals.width, sizeof(globals.width));     // 1280 (Executable) Width
-        WriteMemory((void *)(base + 0x5CC104), &globals.width, sizeof(globals.width));   // 1280 (Executable) Window Width
-        WriteMemory((void *)(base + 0x600184), &globals.width, sizeof(globals.width));   // 1920 (Memory) Necessary
-        WriteMemory((void *)(base + 0x5BFC3D), &globals.height, sizeof(globals.height)); // 720 (Executable) Window Height
-        WriteMemory((void *)(base + 0x585E8D), &globals.width, sizeof(globals.width));   // 1280 (Executable) HUD Fix
-        WriteMemory((void *)(base + 0x5C1D8C), &globals.width, sizeof(globals.width));   // 1280 (Executable) Rage HUD Fix
-
-        uint8_t aspectRatioPatch[6] = {0xDD, 0x05};
-        auto aspectRatioPointer = &globals.aspectRatio;
-        memcpy(&aspectRatioPatch[2], &aspectRatioPointer, 4);
-        float cutsceneAspectRatio = (float)globals.width / (float)globals.height;
-        float multiplayerAspectRatio = (float)(globals.width * 2) / (float)globals.height;
-
-        WriteMemory((void *)(base + 0x22DD1F), &aspectRatioPatch, sizeof(aspectRatioPatch));             // Aspect Ratio
-        WriteMemory((void *)(base + 0x61DF10), &cutsceneAspectRatio, sizeof(cutsceneAspectRatio));       // Fixed Cutscene Aspect Ratio
-        WriteMemory((void *)(base + 0x61DF18), &multiplayerAspectRatio, sizeof(multiplayerAspectRatio)); // Fixed Multiplayer Cutscene Aspect Ratio
-
-        _AspectRatio = (function_22DC70_t *)(base + 0x22DC70);
-
-        if (!(globals.windowed))
-        {
-            uint8_t hideCursorPatch = 0x01;
-            WriteMemory((void *)(base + 0x5CA253), &hideCursorPatch, sizeof(hideCursorPatch)); // Hide Cursor When Fullscreen
-        }
-
-        DetourTransactionBegin();
-        DetourUpdateThread(GetCurrentThread());
-        DetourAttach(&(PVOID &)_Direct3DCreate9, HookedDirect3DCreate9);
-        DetourAttach(&(PVOID &)_RegQueryValueExA, HookedRegQueryValueExA);
-        DetourAttach(&(PVOID &)_RegSetValueExA, HookedRegSetValueExA);
-        if (!(globals.windowed))
-        {
-            DetourAttach(&(PVOID &)_GetSystemMetrics, HookedGetSystemMetrics);
-        }
-        DetourAttach(&(PVOID &)_AspectRatio, HookedAspectRatio);
-        DetourTransactionCommit();
+    case DLL_PROCESS_DETACH:
+        // Perform any necessary cleanup.
         break;
     }
-    return TRUE;
+    return TRUE; // Successful DLL_PROCESS_ATTACH.
 }
